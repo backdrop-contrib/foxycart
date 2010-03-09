91b8edd71af062c6913f8f57cd9bc91a79ec4066
kilogauss <kilogauss@135110.no-reply.drupal.org>
2010-03-09 03:09:54 +0000

    by kilogauss: Initial commit for FoxyCart Integration module

---
+++ foxycart-full-cart-block.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+// $Id$ 
+?>
+<div id="fc_cart">
+<h2>Your Cart</h2>
+<div class="fc_clear"></div>
+<table>
+<thead>
+<th>item</th>
+<th>qty</th>
+<th>price</th>
+</thead>
+<tbody id="cart_content">
+</tbody>
+</table>
+<a href="https://<?php echo $fc_domain; ?>.foxycart.com/cart?cart=checkout" id="fc_checkout_link">Check Out</a>
+<a href="https://<?php echo $fc_domain; ?>.foxycart.com/cart?cart=view" class="foxycart" style="float:right">Edit Cart</a>
+<div class="fc_clear"></div>
+<?php echo $logo; ?>
+</div>
---
+++ foxycart-summary-cart-block.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+// $Id$ 
+
+?>
+<div id="fc_minicart" style="display: none">Your Cart: 
+  <a href="https://<?php echo $fc_domain ?>.foxycart.com/cart?cart=view" class="foxycart">
+  	<span id="fc_quantity">0</span> item(s). 
+  	$<span id="fc_total_price">0.00</span> 
+  </a>
+</div>
